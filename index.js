0619eb356cca0e8b112db6b913b7ee98a4a45e97
Stavros Zavrakas <szavrakas@cambridge.org>
2016-09-27 23:38:11 +0100

    grevil router init

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+const consumes = [];
+const provides = ['grevil.router'];
+
+function setup(options, imports, register) {
+  var router = express.Router();
+
+  router.get('/', function (req, res) {
+    res.json({home: 'home'});
+  });
+
+  return register(null, {
+    'grevil.router': router
+  });
+}
+
+setup.consumes = consumes;
+setup.provides = provides;
+
+module.exports = setup;
